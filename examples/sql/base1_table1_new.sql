eecb01633fadce70a149e35bc72d6f4952165d01
James Dunham <jamesdunham@users.noreply.github.com>
2025-09-29 13:12:40 -0400

    Appease linter

--- examples/sql/base1_table1_new.sql
+++ examples/sql/base1_table1_new.sql
@@ -1,1 +1,3 @@
-SELECT * FROM {{ params.staging_dataset }}.{{ params.staging_table_name }} WHERE foo IS NOT NULL
+SELECT *
+FROM {{ params.staging_dataset }}.{{ params.staging_table_name }}
+WHERE foo IS NOT NULL
